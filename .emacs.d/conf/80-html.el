03503508f8ac7b93ed63c7d3137d2b96eee7ab56
Taro Yamashita <taro@panther-king.com>
2014-05-19 08:01:32 +0900

    web-modeの設定を色々と間違えていた

--- .emacs.d/conf/80-html.el
+++ .emacs.d/conf/80-html.el
@@ -9,15 +9,15 @@
 ;; (add-hook 'html-mode-hook 'my-html-mode-hook)
 
 (when (require 'web-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.\\(html\\|php\\|pt\\|mak\\)$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.\\(html\\|pt\\|mak\\)$" . web-mode))
   (setq web-mode-engines-alist
         '(("blade" . "\\.blade\\.")))
   (defun my-web-mode-hook ()
     (define-key web-mode-map (kbd "C-;") nil)
     (define-key web-mode-map (kbd "C-c C-;") 'web-mode-comment-or-uncomment)
     (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2))
+    (setq web-mode-code-indent-offset 2)
+    (setq web-mode-css-indent-offset 2))
   (add-hook 'web-mode-hook 'my-web-mode-hook)
   (custom-set-faces
    '(web-mode-doctype-face
