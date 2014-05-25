b8ab382df42f760f9a86debcadcad165996ab190
Taro Yamashita <taro@panther-king.com>
2014-05-26 07:44:04 +0900

    js2-modeがいい感じになってた

--- .emacs.d/conf/50-javascript.el
+++ .emacs.d/conf/50-javascript.el
@@ -1,21 +1,12 @@
 ;; JavaScript settings
-(defun my-js-mode-hook ()
-  (setq js-indent-level 2
-        js-expr-indent-offset 2
-        indent-tabs-mode nil)
-
-  (define-key js-mode-map (kbd "=") (smartchr '(" = " " === " "=" " == ")))
-  (define-key js-mode-map (kbd "!") (smartchr '("!" " !== " " != ")))
-
-  (defun my-js-indent-line ()
-    (interactive)
-    (let* ((parse-status (save-excursion (syntax-ppss (point-at-bol))))
-           (offset (- (current-column) (current-indentation)))
-           (indentation (js--proper-indentation parse-status)))
-      (back-to-indentation)
-      (if (looking-at "case\\s-")
-          (indent-line-to (+ indentation 2))
-        (js-indent-line))
-      (when (> offset 0) (forward-char offset))))
-  (set (make-local-variable 'indent-line-function) 'my-js-indent-line))
-(add-hook 'js-mode-hook 'my-js-mode-hook)
+(when (require 'js2-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+  (defun my-js2-mode-hook ()
+    (setq js2-basic-offset 2)
+    (define-key js2-mode-map (kbd "=") (smartchr '(" = " " === " "=" " == ")))
+    (define-key js2-mode-map (kbd "!") (smartchr '("!" " !== " " != ")))
+    (define-key js2-mode-map (kbd "+") (smartchr '("+" "++" " += ")))
+    (define-key js2-mode-map (kbd "-") (smartchr '("-" "--" " -= ")))
+    (define-key js2-mode-map (kbd "<") (smartchr '("<" " < " " <= ")))
+    (define-key js2-mode-map (kbd ">") (smartchr '(">" " > " " >= "))))
+  (add-hook 'js2-mode-hook 'my-js2-mode-hook))
