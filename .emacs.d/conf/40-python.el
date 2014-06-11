7db7609fdcabe9d7cb7680f70c89450d64e85ca1
Taro Yamashita <taro@panther-king.com>
2014-06-12 08:26:45 +0900

    flymakeで快適コーディングライフ

--- .emacs.d/conf/40-python.el
+++ .emacs.d/conf/40-python.el
@@ -14,19 +14,3 @@
     (define-key python-mode-map (kbd "-") (smartchr '("-" " -= ")))
     (define-key python-mode-map (kbd "+") (smartchr '("+" " += "))))
   (add-hook 'python-mode-hook 'my-python-mode-hook))
-
-;; flymake for Python
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "/usr/local/bin/pychecker" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-(load-library "flymake-cursor")
-(global-set-key [f10] 'flymake-goto-next-error)
-(global-set-key [f11] 'flymake-goto-prev-error)
