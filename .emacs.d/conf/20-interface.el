8c7f3ffecab05c7fb46112c57cd27d2a0871ea84
Taro Yamashita <taro@panther-king.com>
2014-07-23 21:25:50 +0900

    タブもハイライトさせたい

--- .emacs.d/conf/20-interface.el
+++ .emacs.d/conf/20-interface.el
@@ -57,9 +57,11 @@
   (setq whitespace-style
         '(face tabs tab-mark spaces space-mark))
   (setq whitespace-space-regexp "\\(\u0020+\\|\u3000+\\)")
+  (setq whitespace-tab-regexp "\\(\u0009+\\)")
   (setq whitespace-display-mappings
         '((space-mark ?\u0020 [?\uff65])
-          (space-mark ?\u3000 [?\u25a1])))
+          (space-mark ?\u3000 [?\u25a1])
+          (tab-mark ?\u0009 [?\xBB ?\t])))
   (set-face-foreground 'whitespace-space "#666666")
   (set-face-background 'whitespace-space 'nil)
   (set-face-foreground 'whitespace-tab "#666666")
@@ -81,7 +83,6 @@
     ""))
 (add-to-list 'default-mode-line-format
              '(:eval (count-lines-and-chars)))
-
 
 ;; Directory tree settings
 ;; git clone https://github.com/m2ym/direx-el.git
