196fc8c5461953985c2c34bbccb3926cbdf5dec9
Taro Yamashita <taro@panther-king.com>
2014-08-19 22:40:00 +0900

    PHPの配列インデントが期待どおりになるように

--- .emacs.d/conf/30-php.el
+++ .emacs.d/conf/30-php.el
@@ -7,12 +7,21 @@
     (setq php-manual-url "http://jp.php.net/manual/ja/")
 
     ;; indent settings
+    ;; @see http://blog.be-open.net/emacs/php-mode%E9%85%8D%E5%88%97%E3%82%A4%E3%83%B3%E3%83%87%E3%83%B3%E3%83%88/
+    (defun ywb-php-lineup-arglist-intro (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (+ (current-column) c-basic-offset))))
+    (defun ywb-php-lineup-arglist-close (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (current-column))))
     (setq php-mode-force-pear nil)
-    (setq indent-tabs-mode nil)
+    (setq indent-tabs-mode t)
     (setq c-basic-offset 4)
     (c-set-offset 'case-label '+)
-    (c-set-offset 'arglist-intro '+)
-    (c-set-offset 'arglist-close 0)
+    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)
 
     ;; keyboard settings
     (define-key php-mode-map (kbd "=") (smartchr '(" = " " === " "=" " == ")))
@@ -30,7 +39,6 @@
         (make-variable-buffer-local 'ac-sources)
         (add-to-list 'ac-sources 'ac-source-php-completion)
         (auto-complete-mode t))))
-
   (add-hook 'php-mode-hook 'my-php-mode-hook))
 
 ;; (when (require 'mmm-auto)
