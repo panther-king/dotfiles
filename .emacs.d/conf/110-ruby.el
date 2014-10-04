aa40839dcfd6de0ad5991ce58342ae67c72f71e3
Taro Yamashita <taro@panther-king.com>
2014-10-04 22:42:08 +0900

    Rubyのハッシュを改行込みで定義する時のインデントが気持ち悪い

--- .emacs.d/conf/110-ruby.el
+++ .emacs.d/conf/110-ruby.el
@@ -4,6 +4,21 @@
 (add-to-list 'auto-mode-alist '("\\.\\(rb\\|rabl\\|jbuilder\\)$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\(Capfile\\|Gemfile\\|Guardfile\\)$" . ruby-mode))
 
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 (defun my-ruby-mode-hook()
   (setq ruby-deep-indent-paren-style nil)
   (define-key ruby-mode-map (kbd "=") (smartchr '(" = " " == " "=")))
