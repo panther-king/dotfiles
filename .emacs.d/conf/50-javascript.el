81dcb80fc6138668c270d17c6073ca9d46973a92
Taro Yamashita <taro@panther-king.com>
2015-01-01 16:40:39 +0900

    CoffeeScriptも手を出すハメに...

--- .emacs.d/conf/50-javascript.el
+++ .emacs.d/conf/50-javascript.el
@@ -10,3 +10,16 @@
     (define-key js2-mode-map (kbd "<") (smartchr '("<" " < " " <= ")))
     (define-key js2-mode-map (kbd ">") (smartchr '(">" " > " " >= "))))
   (add-hook 'js2-mode-hook 'my-js2-mode-hook))
+
+;; CoffeeScript settings
+(autoload 'coffee-mode "coffee-mode"
+  "Major mode for editing CoffeeScript." t)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+
+(defun my-coffee-mode-hook ()
+  (setq coffee-tab-width 2)
+  (define-key coffee-mode-map (kbd "=") (smartchr '(" = " " == " "=")))
+  (define-key coffee-mode-map (kbd "!") (smartchr '("!" " != ")))
+  (define-key coffee-mode-map (kbd ">") (smartchr '(" -> " " > " " => " ">")))
+  (define-key coffee-mode-map (kbd "{") (smartchr '("{`!!'}" "{"))))
+(add-hook 'coffee-mode-hook 'my-coffee-mode-hook)
