d3f4f7db3abb9088e119734ab2958d2c2c6f08dc
Taro Yamashita <taro@panther-king.com>
2015-03-11 07:52:02 +0900

    終了時は確認不要

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -321,7 +321,7 @@ c.TerminalInteractiveShell.banner1 = ''
 # Set to confirm when you try to exit IPython with an EOF (Control-D in Unix,
 # Control-Z/Enter in Windows). By typing 'exit' or 'quit', you can force a
 # direct exit without any confirmation.
-# c.TerminalInteractiveShell.confirm_exit = True
+c.TerminalInteractiveShell.confirm_exit = False
 
 # Autoindent IPython code entered interactively.
 # c.TerminalInteractiveShell.autoindent = True
