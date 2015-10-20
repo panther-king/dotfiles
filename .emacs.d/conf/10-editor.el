54b9c5715aa895fc361737b6b8f978cf3058d829
Taro Yamashita <taro.ymst@gmail.com>
2015-10-21 07:26:49 +0900

    今さらながらmigemoを導入

--- .emacs.d/conf/10-editor.el
+++ .emacs.d/conf/10-editor.el
@@ -46,3 +46,15 @@
 
 ;; Init flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; migemo settings
+(when (require 'migemo nil t)
+  (setq migemo-command "cmigemo")
+  (setq migemo-options '("-q" "--emacs"))
+  (setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-coding-system 'utf-8-unix)
+  (load-library "migemo")
+  (migemo-init))
+
