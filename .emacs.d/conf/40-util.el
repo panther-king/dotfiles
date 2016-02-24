d10ef81a60c144a3972a478f9beebb6f10b0a4b0
Taro Yamashita <taro.ymst@gmail.com>
2016-02-25 08:27:25 +0900

    companyという補完ライブラリの見た目が残念だったので...
    @see http://qiita.com/syohex/items/8d21d7422f14e9b53b17

--- .emacs.d/conf/40-util.el
+++ .emacs.d/conf/40-util.el
@@ -126,3 +126,28 @@
 ;; strict input settings
 (when (require 'drill-instructor nil t)
   (setq drill-instructor-global t))
+
+;; company style and key-bind settings
+(when (require 'company nil t)
+  (set-face-attribute 'company-tooltip nil
+                      :foreground "black" :background "lightgrey")
+  (set-face-attribute 'company-tooltip-common nil
+                      :foreground "black" :background "lightgrey")
+  (set-face-attribute 'company-tooltip-common-selection nil
+                      :foreground "white" :background "steelblue")
+  (set-face-attribute 'company-tooltip-selection nil
+                      :foreground "black" :background "steelblue")
+  (set-face-attribute 'company-preview-common nil
+                      :background nil :foreground "lightgrey" :underline t)
+  (set-face-attribute 'company-scrollbar-fg nil
+                      :background "orange")
+  (set-face-attribute 'company-scrollbar-bg nil
+                      :background "gray40")
+
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous)
+
+  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+  (define-key company-active-map (kbd "C-i") 'company-complete-selection))
