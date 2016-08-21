e4e6f5114f646b07209d424878fe5a7aa8afec4e
Taro Yamashita <taro.ymst@gmail.com>
2016-08-22 07:01:10 +0900

    エディタの基本設定をまとめた

---
+++ .emacs.d/conf/editor.el
@@ -0,0 +1,42 @@
+;; グローバルに利用するキーバインド
+(define-key global-map (kbd "C-h") 'delete-backward-char)
+(define-key global-map (kbd "C-'") 'set-mark-command)
+
+;; タブインデントは利用しない
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+;; 行操作設定
+(setq-default kill-whole-line t)
+(setq-default next-line-add-newlines nil)
+(setq-default require-final-newline t)
+
+;; 現在行のハイライトカスタマイズ
+(defface hlline-face
+  '((((class color) (background dark))
+     (:background "gray15" t))
+    (((class color) (background light))
+     (:background "ForestGreen" t))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(global-hl-line-mode t)
+
+;; 対応するカッコの強調
+(setq show-paren-delay 0)
+(show-paren-mode t)
+
+;; 連続するスペースの可視化
+(setq-default show-trailing-whitespace t)
+
+;; 行末のスペースは保存時に削除
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; 補完
+(icomplete-mode 1)
+(global-set-key (kbd "C-j") 'dabbrev-expand)
+(setq read-buffer-completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+
+;; diredでのファイル名変更
+(define-key dired-mode-map (kbd "r") 'wdired-change-to-wdired-mode)
