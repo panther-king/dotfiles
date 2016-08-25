72e1fe5e7e96196f18d493b844a20cb8fbf42cca
Taro Yamashita <taro.ymst@gmail.com>
2016-08-26 07:24:42 +0900

    Rust1.11リリース

--- .emacs.d/conf/rust.el
+++ .emacs.d/conf/rust.el
@@ -18,12 +18,12 @@
 (el-get-bundle cargo
   :features cargo
   (with-eval-after-load-feature 'cargo
-    (add-hook 'rust-mode-hook #'cargo-minor-mode)))
+    (add-hook 'rust-mode-hook 'cargo-minor-mode)))
 
 (el-get-bundle racer
   :features racer
   (with-eval-after-load-feature 'racer-mode
-    (setq racer-rust-src-path "~/.racer-src/rustc-1.10.0/src/")
+    (setq racer-rust-src-path "~/.racer-src/rustc-1.11.0/src/")
     (add-hook 'rust-mode-hook #'racer-mode))
   (with-eval-after-load-feature 'company
     (add-hook 'racer-mode-hook #'company-mode)))
