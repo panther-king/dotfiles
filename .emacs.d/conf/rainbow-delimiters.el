6882811c1d36a7c4d42e5e1bae7ed254707d0526
Taro Yamashita <taro.ymst@gmail.com>
2016-08-26 07:24:55 +0900

    rainbow-delimiter-modeの初期値だと色付けが見にくい

--- .emacs.d/conf/rainbow-delimiters.el
+++ .emacs.d/conf/rainbow-delimiters.el
@@ -2,4 +2,14 @@
 (el-get-bundle rainbow-delimiters
   :features rainbow-delimiters
   (with-eval-after-load-feature 'rainbow-delimiters
-    (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)))
+    (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+    (require 'cl-lib)
+    (require 'color)
+    (defun rainbow-delimiters-using-stronger-colors ()
+      (interactive)
+      (cl-loop
+       for index from 1 to rainbow-delimiters-max-face-count
+       do
+       (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+         (cl-callf color-saturate-name (face-foreground face) 30))))
+    (add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors)))
