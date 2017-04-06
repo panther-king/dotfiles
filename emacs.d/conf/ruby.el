d03123f212af5497017e4af9be5774c4c91d2153
Taro Yamashita <taro.ymst@gmail.com>
2017-04-06 14:07:36 +0900

    再びruby触るようになったので

--- emacs.d/conf/ruby.el
+++ emacs.d/conf/ruby.el
@@ -1,5 +1,14 @@
 ;; Ruby
 (use-package ruby-mode
+  :config
+  (bind-key "'" (smartchr '("'`!!''" "'")) ruby-mode-map)
+  (bind-key "=" (smartchr '(" = " " == " "=")) ruby-mode-map)
+  (bind-key "!" (smartchr '("!" " != ")) ruby-mode-map)
+  (bind-key "-" (smartchr '("-" " - " " -= ")) ruby-mode-map)
+  (bind-key "+" (smartchr '("+" " + " " += ")) ruby-mode-map)
+  (bind-key ">" (smartchr '(">" " => " " > ")) ruby-mode-map)
+  (bind-key "<" (smartchr '("<" " <<< " " < ")) ruby-mode-map)
+
   :mode (("\\.rb$" . ruby-mode)
          ("\\.rake$" . ruby-mode)
          ("\\.jbuilder$" . ruby-mode)
