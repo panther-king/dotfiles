fb9e7f60275141af669932237c266a01b0e34dba
Taro Yamashita <taro.ymst@gmail.com>
2017-10-06 12:51:31 +0900

    magit-blameをキーバインドで実行できるように

--- emacs.d/conf/magit.el
+++ emacs.d/conf/magit.el
@@ -1,6 +1,7 @@
 ;; magit
 (use-package magit
-  :bind (("C-M-g" . magit-status))
+  :bind (("C-M-g" . magit-status)
+         ("C-c m b" . magit-blame))
   :config
   (defadvice git-commit-commit (after move-to-magit-buffer activate)
     (delete-window))
