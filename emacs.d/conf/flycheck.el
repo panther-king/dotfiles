82ea2277c5d3d7026ed928fd31a14cde4583c976
Taro Yamashita <taro.ymst@gmail.com>
2019-03-18 10:10:05 +0900

    flycheckの設定を見直し

--- emacs.d/conf/flycheck.el
+++ emacs.d/conf/flycheck.el
@@ -3,14 +3,14 @@
   :ensure t
   :bind (("<f12>" . flycheck-next-error)
          ("<f11>" . flycheck-previous-error))
-  :hook (after-init-hook . global-flycheck-mode)
   :config
-  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+  (add-hook 'after-init-hook 'global-flycheck-mode))
 
 ;; ヒントはポップアップで表示させる
 (use-package flycheck-pos-tip
   :ensure t
-  :requires flycheck
+  :after flycheck
   :config
   (setq flycheck-pos-tip-timeout 16)
   (custom-set-variables '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
