33122ce75da589bc7559201d4dad10362542993d
Taro Yamashita <taro.ymst@gmail.com>
2019-04-23 09:04:37 +0900

    ペースト時のカラーを見直した(再

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -182,7 +182,7 @@
   (show-paren-style 'mixed)
   (show-paren-when-point-inside-paren t)
   (show-paren-when-point-in-periphery t)
-  :custom-face (show-paren-match ((nil (:background "#44475a" :foreground "#f1fa8c"))))
+  :custom-face (show-paren-match ((nil (:background "#44475a" :foreground "violet"))))
   :ensure nil
   :init (show-paren-mode t))
 
