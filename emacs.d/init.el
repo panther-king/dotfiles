454780dc6b68bfc7132c26c1ad7fc3efb3da6f4d
Taro Yamashita <taro.ymst@gmail.com>
2021-01-27 14:17:49 +0900

    elm-modeのadd-listがエラーになっていた。

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -442,7 +442,9 @@
   (bind-key ":" (smartchr '(" : " " :: " ":")) elm-mode-map)
   (bind-key "|" (smartchr '(" | " "|> " " <|" "|")) elm-mode-map)
   (bind-key "\"" (smartchr '("\"`!!'\"" "\"" "\"\"\"`!!'\"\"\"")) elm-mode-map)
-  (add-to-ilst 'company-backends 'elm-company)
+  (with-eval-after-load 'company
+    (add-to-list 'company-backends 'company-elm)
+    (add-hook 'elm-mode-hook #'elm-oracle-setup-completion))
   (add-hook 'elm-mode-hook 'elm-format-on-save-mode)
   :ensure t)
 
