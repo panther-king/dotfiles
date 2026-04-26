10063b563397f6953ea25a6d9203057f9c01b261
Taro Yamashita <taro.ymst@gmail.com>
2026-04-26 21:04:07 +0900

    今回こそ xmonad に乗り換える

---
+++ .config/xmonad/xmonad.hs
@@ -0,0 +1,66 @@
+import XMonad
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Layout.Spacing
+import XMonad.StackSet qualified as W
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.SpawnOnce (spawnOnce)
+
+main :: IO ()
+main =
+  xmonad . ewmhFullscreen . ewmh . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey $
+    def
+      { borderWidth = 2,
+        -- ウインドウのフルスクリーントグルを可能にしておく
+        layoutHook = mkToggle (single FULL) $ spacingWithEdge 4 $ layoutHook def,
+        modMask = mod4Mask,
+        startupHook = myStartupHook,
+        terminal = "alacritty"
+      }
+      `additionalKeysP` myAdditionalKeysP
+
+myAdditionalKeysP :: [(String, X ())]
+myAdditionalKeysP =
+  [
+    ("M-<Return>", spawn "alacritty"),
+    ("M-S-<Return>", spawn "i3lock -i $HOME/Pictures/wallpaper-catppuccin.png"),
+    -- ウインドウ間の移動
+    ("M-n", windows W.focusDown),
+    ("M-p", windows W.focusUp),
+    -- フルスクリーンのトグル
+    ("M-S-f", sendMessage $ Toggle FULL),
+    -- フォーカスウインドウをマスターにスワップ
+    ("M-S-m", windows W.swapMaster),
+    -- rofi
+    ("M-r", spawn "rofi -show drun"),
+    ("M-S-r", spawn "rofi -show window"),
+    -- スクリーンショット
+    ("<Print>", spawn "import -window root $HOME/Pictures/screenshot$(date \"+%Y%m%d%H%M%S\").jpg"),
+    -- 画面のバックライト
+    ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 5"),
+    ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 5"),
+    -- ボリューム
+    ("<XF86AudioMute>", spawn "amixer sset Master mute"),
+    ("<XF86AudioLowerVolume>", spawn "amixer set Master 1%-"),
+    ("<XF86AudioRaiseVolume>", spawn "amixer set Master 1%+")
+  ]
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOnce "feh --no-fehbg --bg-scale $HOME/Pictures/wallpaper-catppuccin.png"
+  spawnOnce "picom --config $HOME/.config/picom/picom.conf"
+  spawnOnce "nm-applet"
+  spawnOnce "fcitx5"
+  spawnOnce "dunst"
+  spawnOnce "parcellite"
+
+myXmobarPP :: PP
+myXmobarPP =
+  xmobarPP
+    { ppCurrent = xmobarColor "#181825" "#74c7ec" . wrap " " " ",
+      ppHidden = xmobarColor "#585b70" "" . wrap " " " ",
+      ppUrgent = xmobarColor "#f38ba8" "" . wrap " " " "
+    }
