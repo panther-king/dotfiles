d342ae43fa8961cf7c89f8ee57b54f067207ede2
Taro Yamashita <taro.ymst@gmail.com>
2026-05-20 20:29:41 +0900

    git-gutter から diff-hl に乗り換えてみた

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -525,12 +525,14 @@
 ;;
 
 ;; ファイルの編集状況をフリンジに表示する
-(use-package git-gutter
-  :config (global-git-gutter-mode t)
+(use-package diff-hl
+  :bind
+  (("<f9>" . diff-hl-previous-hunk)
+   ("<f10>" . diff-hl-next-hunk))
+  :config
+  (global-diff-hl-mode)
   :custom
-  (git-gutter:modified-sign "~")   ;; 変更
-  (git-gutter:added-sign "+")      ;; 追加
-  (git-gutter:deleted-sign "!"))   ;; 削除
+  (diff-hl-update-async t))
 
 ;; gitの設定ファイルメジャーモード
 (use-package git-modes)
