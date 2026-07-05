2f5cee0a67d56cdc1bb0311ee3f58c23a004043a
Taro Yamashita <taro.ymst@gmail.com>
2026-07-05 21:03:30 +0900

    PapirusIcon テーマの追加漏れ

--- nix/desktop.nix
+++ nix/desktop.nix
@@ -1,4 +1,12 @@
 { pkgs, ... }: {
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+  };
+
   # デスクトップ環境のみで利用するパッケージ
   home.packages = with pkgs; [
     brightnessctl
